c11e1307a6442f00826a722a003756ac508fa22a
M Xi <bongollmaria@gmail.com>
2025-04-08 13:07:54 +0200

    feat/Create controller with post method

--- src/main/java/com/periodico/newspaper/controller/ArticleController.java
+++ src/main/java/com/periodico/newspaper/controller/ArticleController.java
@@ -1,5 +1,30 @@
 package com.periodico.newspaper.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.periodico.newspaper.model.Article;
+import com.periodico.newspaper.service.ArticleService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("api/v1/articles")
 public class ArticleController {
-  
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createArticle(@Valid @RequestBody Article article) {
+        return articleService.createArticle(article);
+    }
 }
