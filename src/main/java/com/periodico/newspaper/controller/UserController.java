b48b9ffc4d0637f1e5504ad5bd63a17153f82b43
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-04-09 12:45:35 +0200

    feature/create user controller

---
+++ src/main/java/com/periodico/newspaper/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.periodico.newspaper.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.periodico.newspaper.model.User;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+    return userService.createUser(user);
+  }
+}
