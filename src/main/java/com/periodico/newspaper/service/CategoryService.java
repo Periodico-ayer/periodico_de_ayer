57f525f813bcaf5dab6a4d877e3ecb4e00d9ac05
RieraDiPe <rieradipe@gmail.com>
2025-04-09 12:57:43 +0200

    fix/created a service, category and user service

---
+++ src/main/java/com/periodico/newspaper/service/CategoryService.java
@@ -0,0 +1,26 @@
+package com.periodico.newspaper.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.periodico.newspaper.model.Category;
+import com.periodico.newspaper.repository.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public ResponseEntity<Object> createCategory(Category category) {
+        categoryRepository.save(category);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+
+    }
+
+}
---
+++ src/main/java/com/periodico/newspaper/service/UserService.java
@@ -0,0 +1,25 @@
+package com.periodico.newspaper.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.periodico.newspaper.model.User;
+import com.periodico.newspaper.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<Object> createUser(User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+
+    }
+
+}
